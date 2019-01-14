61826ae56c8801655d2bf402fd18f79b4531fc7a
Brent Hays <brenthays@Brents-MacBook-Pro.local>
2019-01-14 13:04:48 -0600

    conditionally shows box score link when game is no longer in scheduled state

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -4,23 +4,29 @@ import '../styles/ScoreCard.css';
 
 class ScoreCard extends Component {
   render() {
+    let game = this.props.game
+
+    let boxScoreLink = (
+      <span>| <Link to={'/game/'+this.props.game.gamePk}>Box Score</Link></span>
+    )
+
     return (
       <div className="ScoreCard card">
         <div className="card-body text-center">
           <div className="row">
             <div className="col-5">
-              <p className="card-text">{this.props.game.teams.away.team.name}</p>
-              <p className="card-text Score">{this.props.game.teams.away.score}</p>
+              <p className="card-text">{game.teams.away.team.name}</p>
+              <p className="card-text Score">{game.teams.away.score}</p>
             </div>
             <div className="col-2 ScoreSeparator">@</div>
             <div className="col-5">
-              <p className="card-text">{this.props.game.teams.home.team.name}</p>
-              <p className="card-text Score">{this.props.game.teams.home.score}</p>
+              <p className="card-text">{game.teams.home.team.name}</p>
+              <p className="card-text Score">{game.teams.home.score}</p>
             </div>
           </div>
           <div className="row">
             <div className="col-12 text-muted">
-              {this.props.game.status.detailedState} | <Link to={'/game/'+this.props.game.gamePk}>Box Score</Link>
+              {game.status.detailedState}&nbsp;{game.status.detailedState !== 'Scheduled' ? boxScoreLink : ''}
             </div>
           </div>
         </div>
